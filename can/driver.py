167120f0f876a9bf5679965b59a1c5d488c00c69
leocances <leocances@gmail.com>
2022-12-21 10:56:48 +0100

    feat: Add 01net website

--- can/driver.py
+++ can/driver.py
@@ -33,16 +33,49 @@ class Korben(Website):
         )
 
 
+class Zero1Net(Website):
+    def __init__(self):
+        self.name = "01net"
+        self.url = "https://www.01net.com/actualites/"
+        self.category = "tech"
+
+    def fetch_last_article(self):
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # Find the first section tag and the direct main tag inside
+        section = soup.find("section")
+        main = section.find("main")
+
+        # Inside the main, fetch the href from the first link
+        link = main.find("a")
+        url = link.get("href")
+
+        # Inside this link, fetch the first div and get the text only
+        div = link.find("div")
+        title = div.text
+
+        return Article(
+            self,
+            title,
+            "NaN",
+            datetime.now().date(),
+            url,
+        )
+        
+
+
 def driver_factory(driver_name: str):
     mapper = dict(
         korben=Korben,
+        zero1net=Zero1Net,
     )
 
     return mapper[driver_name]()
 
 
 if __name__ == "__main__":
-    korben = Korben()
-    art = korben.fetch_last_article()
+    website = driver_factory("zero1net")
+    art = website.fetch_last_article()
     print(art)
     print(art.url)
